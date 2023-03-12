a9570fc90d80e4d4c67d065bb8421d8468af1268
Christian Maciosek <ohioteppich@gmail.com>
2023-03-12 20:56:18 +0100

    MBL-17 Basics of translation

--- frontend/lib/features/home/home.widget.dart
+++ frontend/lib/features/home/home.widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Hellow World');
+    return Center(
+      child: Text(AppLocalizations.of(context)!.helloWorld),
+    );
   }
 }
