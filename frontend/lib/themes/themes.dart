e4e88017d228064a3a74e5fea6bb56be087583ef
Christian Maciosek <ohioteppich@gmail.com>
2023-03-12 13:10:29 +0100

    MBL-18 Color and Text Themes added

---
+++ frontend/lib/themes/themes.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+
+class StandardColor {
+  static const Color primary =  Color(0xFFEDE0D8);
+  static const Color secondary = Color(0xFFE1CCBE);
+  
+
+  static const Color accent = Color(0xFF77A0A5);
+  static const Color accentOnSeconday = Color(0xFF5F8084);
+  static const Color accentPrimaryButton = Color(0xFFADC6C9);
+
+  static const Color strokeColor = Color(0xFF757263);
+  static const Color infoIconColor = Color(0xFF3B3B3B);
+
+  static const Color textColor = Color(0xFF3B3B3B);
+  static const Color textContrastColor = Color(0xFFEEEDEB);
+  static const Color headlineAccent = Color(0xFF757263);
+
+}
+
+class StandardText {
+  static TextStyle headline = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.bold,
+    fontSize: 46,
+    color: StandardColor.textColor,
+    height: 44,
+    letterSpacing: 0,
+  );
+
+  static TextStyle headline2 = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.bold,
+    fontSize: 38,
+    color: StandardColor.textColor,
+    height: 37,
+    letterSpacing: 0,
+  );
+
+  static TextStyle headline3 = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.bold,
+    fontSize: 30,
+    color: StandardColor.textColor,
+    height: 31,
+    letterSpacing: 0,
+  );
+
+  static TextStyle headline4 = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.bold,
+    fontSize: 24,
+    color: StandardColor.textColor,
+    height: 25,
+    letterSpacing: 0,
+  );
+
+  static TextStyle headline5 = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.bold,
+    fontSize: 22,
+    color: StandardColor.textColor,
+    height: 23,
+    letterSpacing: 0,
+  );
+
+  static TextStyle headline6 = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.bold,
+    fontSize: 20,
+    color: StandardColor.textColor,
+    height: 21,
+    letterSpacing: 0,
+  );
+
+  static TextStyle subtitle1 = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+    color: StandardColor.textColor,
+    height: 18,
+    letterSpacing: 0,
+  );
+
+  static TextStyle subtitle2 = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    color: StandardColor.textColor,
+    height: 16,
+    letterSpacing: 0,
+  );
+
+  static TextStyle body1 = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.normal,
+    fontSize: 16,
+    color: StandardColor.textColor,
+    height: 20,
+    letterSpacing: 0,
+  );
+
+  static TextStyle body2 = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.normal,
+    fontSize: 14,
+    color: StandardColor.textColor,
+    height: 16,
+    letterSpacing: 0,
+  );
+
+  static TextStyle body1Itali = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.normal,
+    fontStyle: FontStyle.italic,
+    fontSize: 16,
+    color: StandardColor.textColor,
+    height: 20,
+    letterSpacing: 0,
+  );
+
+  static TextStyle body2Itali = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.normal,
+    fontStyle: FontStyle.italic,
+    fontSize: 14,
+    color: StandardColor.textColor,
+    height:16,
+    letterSpacing: 0,
+  );
+
+  static TextStyle body1Bold = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.bold,
+    fontSize: 16,
+    color: StandardColor.textColor,
+    height: 20,
+    letterSpacing: 0,
+  );
+
+  static TextStyle body2Bold = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.bold,
+    fontSize: 14,
+    color: StandardColor.textColor,
+    height: 16,
+    letterSpacing: 0,
+  );
+
+  static TextStyle button = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.bold,
+    fontSize: 16,
+    color: StandardColor.textColor,
+    height: 20,
+    letterSpacing: 0,
+  );
+
+  static TextStyle captionNormal = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.normal,
+    fontSize: 12,
+    color: StandardColor.textColor,
+    height: 16,
+    letterSpacing: 0.2,
+  );
+
+  static TextStyle captionBold = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.bold,
+    fontSize: 12,
+    color: StandardColor.textColor,
+    height: 16,
+    letterSpacing: 0.2,
+  );
+
+  static TextStyle overline = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.normal,
+    fontStyle: FontStyle.italic,
+    fontSize: 12,
+    color: StandardColor.textColor,
+    height: 16,
+    letterSpacing: 0.2,
+  );
+}
