36cc3b73fcb1c0e5db1d97343800dc03617f220e
Unsignedmind <unsignedmind.dev@gmail.com>
2023-03-18 15:44:16 +0100

    MBL-26 change lineendings to lf

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -6,8 +6,8 @@ import 'package:mbl/utils/app-bloc.observer.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 Future main() async {
-  await dotenv.load(fileName: 'assets/.env');
-  Bloc.observer = const SimpleBlocObserver();
+  await dotenv.load(fileName: "assets/.env");
+  Bloc.observer = SimpleBlocObserver();
   runApp(const Main());
 }
 
