c88147f0624397a316fdf7a4036cb4f04fb1a289
Christian Maciosek <ohioteppich@gmail.com>
2023-03-23 14:48:36 +0100

    Blur for Audio Player

--- frontend/lib/features/audio-player/audio_player_screen.widget.dart
+++ frontend/lib/features/audio-player/audio_player_screen.widget.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:just_audio/just_audio.dart';
@@ -6,6 +8,7 @@ import 'package:mbl/features/audio-player/player_button.widget.dart';
 import 'package:mbl/features/widget/media_app_bar.widget.dart';
 import 'package:mbl/repository/models/position_data.model.dart';
 import 'package:mbl/themes/themes.dart';
+import 'package:palette_generator/palette_generator.dart';
 import 'package:rxdart/rxdart.dart';
 
 class AudioPlayerScreen extends StatefulWidget {
@@ -15,7 +18,7 @@ class AudioPlayerScreen extends StatefulWidget {
     required this.speaker,
     required this.audioUrl,
     required this.coverUrl,
-    });
+  });
 
   final String title;
   final String speaker;
@@ -27,26 +30,39 @@ class AudioPlayerScreen extends StatefulWidget {
 }
 
 class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
- late AudioPlayer _player;
- late String _host;
-  
-Stream<PositionData> get _positionDataStream =>
-  Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
-    _player.positionStream,
-    _player.bufferedPositionStream,
-    _player.durationStream,
-    (position, bufferedPosition, duration) => PositionData(
-      position,
-      bufferedPosition,
-      duration ?? Duration.zero,
-    ),
-  );
+  late AudioPlayer _player;
+  late String _host;
+  late PaletteColor colors;
+  late String image;
 
- @override
+  Stream<PositionData> get _positionDataStream =>
+      Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
+        _player.positionStream,
+        _player.bufferedPositionStream,
+        _player.durationStream,
+        (position, bufferedPosition, duration) => PositionData(
+          position,
+          bufferedPosition,
+          duration ?? Duration.zero,
+        ),
+      );
+
+  @override
   void initState() {
     super.initState();
+    colors = PaletteColor(Colors.transparent, 2);
     _host = dotenv.get('HOST');
-    init();    
+    image = 'http://$_host${widget.coverUrl}';
+    _updatePaletts();
+    init();
+  }
+
+  void _updatePaletts() async {
+    final PaletteGenerator generator =
+        await PaletteGenerator.fromImageProvider(NetworkImage(image));
+    colors = generator.lightMutedColor ?? PaletteColor(Colors.transparent, 2);
+
+    setState(() {});
   }
 
   void init() async {
@@ -69,99 +85,147 @@ Stream<PositionData> get _positionDataStream =>
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<PlayerState>(
-      stream: _player.playerStateStream,
-      builder: (context, snapshot) {
-        final playerState = snapshot.data;
-        if (playerState?.processingState == ProcessingState.loading ||
-       playerState?.processingState == ProcessingState.buffering) {
-        return const Scaffold(
-          backgroundColor: StandardColor.primary,
-          body: Center(
-            child: CircularProgressIndicator(
-              color: StandardColor.accentPrimaryButton,
-            ),
-          ),
-        );
-        } else {
-        return Scaffold(
-        backgroundColor: StandardColor.primary,
-        appBar: const MediaAppBar(),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Image.network('http://$_host${widget.coverUrl}'),
-            const SizedBox(height: 30),
-            Container(
-              alignment: Alignment.bottomCenter,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                 color: StandardColor.textContrastColor),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 18.73),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        stream: _player.playerStateStream,
+        builder: (context, snapshot) {
+          final playerState = snapshot.data;
+          if (playerState?.processingState == ProcessingState.loading ||
+              playerState?.processingState == ProcessingState.buffering) {
+            return const Scaffold(
+              backgroundColor: StandardColor.primary,
+              body: Center(
+                child: CircularProgressIndicator(
+                  color: StandardColor.accentPrimaryButton,
+                ),
+              ),
+            );
+          } else {
+            return Scaffold(
+              backgroundColor: StandardColor.primary,
+              appBar: const MediaAppBar(),
+              body: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Stack(
+                    alignment: Alignment.center,
+                    children: <Widget>[
+                      Center(
+                        child: Container(
+                          width: 370,
+                          height: 370,
+                          decoration: BoxDecoration(
+                            color: colors.color.withOpacity(0.75),
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                      ),
+                      BackdropFilter(
+                        filter: ImageFilter.blur(
+                          sigmaX: 30.0,
+                          sigmaY: 30.0,
+                        ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: colors.color.withOpacity(0.75),
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                      ),
+                      Image.network('http://$_host${widget.coverUrl}'),
+                    ],
+                  ),
+                  const SizedBox(height: 30),
+                  Container(
+                    alignment: Alignment.bottomCenter,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: StandardColor.textContrastColor),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 18.73),
+                      child: Column(
                         children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children:[
-                              Text(widget.title, style: StandardText.headline6,),
-                              const SizedBox(height: 5,),
-                              Text(widget.speaker, style: StandardText.subtitle2,),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 20.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      widget.title,
+                                      style: StandardText.headline6,
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Text(
+                                      widget.speaker,
+                                      style: StandardText.subtitle2,
+                                    ),
+                                  ],
+                                ),
+                                const Icon(Icons.favorite_border_outlined)
+                              ],
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 15.0,
+                          ),
+                          CustomProgressBar(
+                              positionDataStream: _positionDataStream,
+                              player: _player),
+                          const SizedBox(
+                            height: 20.0,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  if (_player.position <
+                                      const Duration(seconds: 10)) {
+                                    _player.seek(Duration.zero);
+                                  } else {
+                                    _player.seek(_player.position -
+                                        const Duration(seconds: 10));
+                                  }
+                                },
+                                icon: const Icon(Icons.replay_10_rounded),
+                                iconSize: 60,
+                              ),
+                              const SizedBox(width: 27),
+                              playerButton(playerState, _player),
+                              const SizedBox(width: 27),
+                              IconButton(
+                                onPressed: () {
+                                  if (_player.position >
+                                      _player.duration! -
+                                          const Duration(seconds: 10)) {
+                                    _player.seek(_player.duration);
+                                  } else {
+                                    _player.seek(_player.position +
+                                        const Duration(seconds: 10));
+                                  }
+                                },
+                                icon: const Icon(Icons.forward_10_rounded),
+                                iconSize: 60,
+                              ),
                             ],
                           ),
-                          const Icon(Icons.favorite_border_outlined)
+                          const SizedBox(
+                            height: 80,
+                          )
                         ],
                       ),
                     ),
-                    const SizedBox(height: 15.0,),
-                    CustomProgressBar(positionDataStream: _positionDataStream, player: _player),
-                    const SizedBox(height: 20.0,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          onPressed: () {
-                            if(_player.position < const Duration(seconds: 10)){
-                              _player.seek(Duration.zero);
-                            } else {
-                              _player.seek(_player.position - const Duration(seconds: 10));
-                            } 
-                          },
-                          icon: const Icon(Icons.replay_10_rounded),
-                          iconSize: 60,
-                        ),
-                        const SizedBox(width: 27),
-                        playerButton(playerState, _player),
-                        const SizedBox(width: 27),
-                        IconButton(
-                          onPressed: () {
-                           if(_player.position > _player.duration! - const Duration(seconds: 10)){
-                              _player.seek(_player.duration);
-                            } else {
-                              _player.seek(_player.position + const Duration(seconds: 10));
-                            } 
-                          },
-                          icon: const Icon(Icons.forward_10_rounded),
-                          iconSize: 60,
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 80,)
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
-      );
-       }
-      }
-    );
+            );
+          }
+        });
   }
 }
