ff08b3c4d9e20d43c087e60fdf296ad90a5f1899
Christian Maciosek <ohioteppich@gmail.com>
2023-03-30 12:38:43 +0200

    MBL-30 Added the ListGrid Switch for Pilates

--- frontend/lib/features/pilates/pilates_success.widget.dart
+++ frontend/lib/features/pilates/pilates_success.widget.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mbl/features/audio-player/audio_player_screen.widget.dart';
 import 'package:mbl/features/pilates/bloc/pilates_bloc.dart';
+import 'package:mbl/features/shared/collection_header.widget.dart';
+import 'package:mbl/features/shared/list_grid_switch/list_grid_switch.settings.dart';
 import 'package:mbl/l10n/l10n.dart';
+import 'package:mbl/themes/themes.dart';
 
 class PilatesSuccess extends StatelessWidget {
   const PilatesSuccess({super.key});
@@ -14,37 +17,42 @@ class PilatesSuccess extends StatelessWidget {
       builder: (context, state) {
         switch (state.status) {
           case PilatesStatus.loading:
-            return ElevatedButton(
-              child: const Text('AudioPlayer'),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const AudioPlayerScreen(
-                            title: 'title',
-                            speaker: 'speaker',
-                            audioUrl:
-                                'https://s3.amazonaws.com/scifri-episodes/scifri20181123-episode.mp3',
-                            coverUrl:
-                                'http://192.168.178.41:1337/uploads/MBL_circle_only_1_b38693f243.png')));
-              },
+            return const Center(
+              child: CircularProgressIndicator(
+                color: StandardColor.accentPrimaryButton,
+              ),
             );
           case PilatesStatus.success:
-            return Center(
-              child: ElevatedButton(
-                child: const Text('AudioPlayer'),
-                onPressed: () {
-                  Navigator.push(
+            return Scaffold(
+              backgroundColor: StandardColor.primary,
+              appBar: CollectionHeader(
+                title: l10n.pilates,
+                listGridSwitchSettings: ListGridSwitchSettings(
+                  (viewMode) {
+                    context.read<PilatesBloc>().add(ToggleViewMode(viewMode));
+                  },
+                  state.viewMode,
+                ),
+              ),
+              body: Center(
+                child: ElevatedButton(
+                  child: const Text('AudioPlayer'),
+                  onPressed: () {
+                    Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => const AudioPlayerScreen(
-                              title: 'title',
-                              speaker: 'speaker',
-                              audioUrl:
-                                  'https://s3.amazonaws.com/scifri-episodes/scifri20181123-episode.mp3',
-                              coverUrl:
-                                  'http://192.168.178.41:1337/uploads/MBL_circle_only_1_b38693f243.png')));
-                },
+                        builder: (context) => const AudioPlayerScreen(
+                          title: 'title',
+                          speaker: 'speaker',
+                          audioUrl:
+                              'https://s3.amazonaws.com/scifri-episodes/scifri20181123-episode.mp3',
+                          coverUrl:
+                              'http://192.168.178.41:1337/uploads/MBL_circle_only_1_b38693f243.png',
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
             );
           case PilatesStatus.error:
---
+++ frontend/lib/features/shared/collection_header.widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:mbl/features/shared/list_grid_switch/list_grid_switch.settings.dart';
+import 'package:mbl/features/shared/list_grid_switch/list_grid_switch.widget.dart';
+import 'package:mbl/themes/themes.dart';
+
+class CollectionHeader extends StatelessWidget implements PreferredSizeWidget {
+  const CollectionHeader({
+    super.key,
+    required this.title,
+    required this.listGridSwitchSettings,
+  });
+
+  final String title;
+  final ListGridSwitchSettings listGridSwitchSettings;
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      title: Text(
+        title,
+        style: StandardText.headline3
+            .copyWith(color: StandardColor.headlineAccent),
+      ),
+      actions: <Widget>[
+        ListGridSwitch(listGridSwitchSettings: listGridSwitchSettings),
+        const VerticalDivider(
+          thickness: 1,
+          indent: 15,
+          endIndent: 15,
+          color: StandardColor.strokeColor,
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: const Icon(
+            Icons.filter_alt,
+            color: StandardColor.accent,
+          ),
+        ),
+      ],
+    );
+  }
+}
