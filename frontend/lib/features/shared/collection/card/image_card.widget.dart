2b74c9978f3d4c818319ccd54b1e559a527fadf9
Christian Maciosek <ohioteppich@gmail.com>
2023-04-17 12:27:29 +0200

    MBL-31 Prepared onTap for Video Screen at GridView

--- frontend/lib/features/shared/collection/card/image_card.widget.dart
+++ frontend/lib/features/shared/collection/card/image_card.widget.dart
@@ -14,52 +14,55 @@ class ImageCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        item.image != null && item.image?.url != null
-            ? Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(
-                    image: NetworkImage(item.image?.url ?? ''),
-                    fit: BoxFit.fill,
+    return GestureDetector(
+      onTap: () => item.onClickCallback(),
+      child: Stack(
+        children: [
+          item.image != null && item.image?.url != null
+              ? Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: NetworkImage(item.image?.url ?? ''),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                )
+              : Container(),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.black.withOpacity(0.5),
+                  Colors.transparent,
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 10,
+              top: 10,
+              right: 10,
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  child: Text(
+                    item.title ?? '',
+                    style: StandardText.body1.copyWith(color: Colors.white),
                   ),
                 ),
-              )
-            : Container(),
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Colors.black.withOpacity(0.5),
-                Colors.transparent,
               ],
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(
-            left: 10,
-            top: 10,
-            right: 10,
-          ),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Flexible(
-                child: Text(
-                  item.title ?? '',
-                  style: StandardText.body1.copyWith(color: Colors.white),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
